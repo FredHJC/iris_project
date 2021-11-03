38337059a3ba3acbf60269a79019bca9aefdca84
frederickkkk123 <jh4976@nyu.edu>
2021-11-03 15:40:32 -0400

    normalization update

--- IrisNormalization.py
+++ IrisNormalization.py
@@ -2,14 +2,9 @@
 # coding: utf-8
 import cv2
 import numpy as np
-import glob
-import math
-from scipy.spatial import distance
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import metrics
-
 
 def getRotation(image, degrees):
     # getRotation(image, degree):
@@ -20,46 +15,68 @@ def getRotation(image, degrees):
     """
     res = []
     for degree in degrees:
-      pixels = abs(int(512*degree/360))
-      if degree == 0:
-        res.append(image)
-      elif degree > 0:
-          res.append(np.hstack([image[:,pixels:],image[:,:pixels]]))
-      else:
-          res.append(np.hstack([image[:,(512 - pixels):],image[:,:(512 - pixels)]]))
+        pixels = abs(int(512*degree/360))
+        if degree == 0:
+            res.append(image)
+        elif degree > 0:
+            res.append(np.hstack([image[:,pixels:],image[:,:pixels]]))
+        else:
+            res.append(np.hstack([image[:,(512 - pixels):],image[:,:(512 - pixels)]]))
 
     return res
 
-def IrisNormalization(boundary,centers):
-    target = [img for img in boundary]
-    normalized=[]
-    cent=0
-    for img in target:
-        #load pupil centers and radius of inner circles
-        center_x = centers[cent][0]
-        center_y = centers[cent][1]
-        radius_pupil=int(centers[cent][2])
-        
-        iris_radius = 53 # width of space between inner and outer boundary
-    
-        #define equally spaced interval to iterate over
-        nsamples = 360
-        samples = np.linspace(0,2*np.pi, nsamples)[:-1]
-        polar = np.zeros((iris_radius, nsamples))
-        for r in range(iris_radius):
-            for theta in samples:
-                #get x and y for values on inner boundary
-                x = (r+radius_pupil)*np.cos(theta)+center_x
-                y = (r+radius_pupil)*np.sin(theta)+center_y
-                x=int(x)
-                y=int(y)
+def IrisNormalization(boundary,centers, rotation_flag):
+    def apply_transform(iris_width, theta_range, x_p, y_p, pupil_radius, polar, img):
+        for Y in range(iris_width):
+            for theta in theta_range:
+                # get x and y for values on inner boundary
+                # iris_width + pupil_radius = outer boundary radius
+                outer_radius = Y + pupil_radius
+
+                x = int(x_p + (outer_radius) * np.cos(theta))
+                y = int(y_p + (outer_radius) * np.sin(theta))
+
+
+                # theta = 2*pi*X => X = theta/(2*pi)
+                # Here theta is only one slice angle, need to multiply by 360
+                X  = int((360 * theta)/(2 * np.pi))
+
                 try:
-                #convert coordinates
-                    polar[r][int((theta*nsamples)/(2*np.pi))] = img[y][x]
-                except IndexError: #ignores values which lie out of bounds
+                    # convert polar coordinates to cartesian coordinates
+                    polar[Y][X] = img[y][x]
+
+                # discard values out of boundary
+                except IndexError:
                     pass
                 continue
-        res = cv2.resize(polar,(512,64))
-        normalized.append(res)
-        cent+=1
-    return normalized #returns a list of 64x512 normalized images
+
+            # reshape image into size 64*512 size
+            polar_resize = cv2.resize(polar,(512,64))
+            return polar_resize
+
+
+    # x,y location for pupil and pupil radius
+
+    # x_p = centers[center][0]
+    # y_p = centers[center][1]
+    # pupil_radius = int(centers[center][2])
+
+    # iris width, the distance between inner boundary and outer boundary
+    iris_width = 55
+
+    # define equally spaced interval to iterate over
+    theta_range = np.linspace(0,2*np.pi,360)[:-1]
+
+    # create an empty list of array for polar coordinates
+    # for normalized location(X,Y), X is the 360 sliced circumstance
+    # Y is the iris width
+    polar = np.zeros((iris_width,360))
+
+    tmp = list(zip(boundary,centers))
+
+    normalized = list(map(lambda x: apply_transform(iris_width, theta_range, x[1][0], x[1][1], x[1][2],polar, x[0]), tmp))
+
+    if not rotation_flag:
+        return normalized
+    else:
+        return sum(list(map(lambda p: getRotation(p, [-9,-6,-3,0,3,6,9]), normalized)), [])
